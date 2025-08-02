392d6c2d256e1b5852f88e873295165a095285f5
Rohit22926 <ray548628@gmail.com>
2025-08-03 01:13:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined? "ENTER YOUR SESSION ID":process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined? "IvVG0QDJ#XjfGHA-x2lUBCrAwkhbplXSGGDyXWk3Acut28b3Rb9s":process.env.SESSION_ID,
 MONGODB: process.env.MONGODB || "ENTER YOUR MONGO DB URL"mongodb://mongo:BGjaemqhsshcUtsSYuiKGMJSGLpEDzcH@trolley.proxy.rlwy.net:23060,
 GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyDzyQ7tnQkquWX8bc1y9TE0auCHbC_QQO8",
 tokenm: process.env.GITHUB_TOKEN || "false",
 repo: process.env.GITHUB_REPO || "RAWANA-MD",
 }; 
+I am agree
